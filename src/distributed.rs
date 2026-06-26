7e9bfbe2c462ad462d5547a3e47c6e6b70a490d0
Gautam011004 <divyagautam326@gmail.com>
2026-06-26 23:01:59 +0530

    Testing durability

--- src/distributed.rs
+++ src/distributed.rs
@@ -1,43 +1,61 @@
 use std::{sync::Arc, time::Duration};
 
 use bytes::BytesMut;
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream, sync::Mutex, time::{Instant, sleep}};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::Mutex,
+    time::{Instant, sleep},
+};
 
 use crate::types::{Role, Rpc, ThisNode};
 
-pub async fn request_reader(mut conn: TcpStream, me: Arc<Mutex<ThisNode>>, heartbeat_guard: Arc<Mutex<Instant>>) {
+pub async fn request_reader(
+    mut conn: TcpStream,
+    me: Arc<Mutex<ThisNode>>,
+    heartbeat_guard: Arc<Mutex<Instant>>,
+) {
     let mut buf = BytesMut::new();
     loop {
         if let Some(s) = buf.iter().position(|x| *x == b'\n') {
-            vote_caster(&mut buf, s, me.clone(), heartbeat_guard.clone()).await;
+            vote_caster(&mut buf, s, me.clone(), heartbeat_guard.clone(), &mut conn).await;
         }
-        conn.read_buf(&mut buf).await.unwrap();
-        sleep(Duration::from_secs(5)).await;
+        let n = conn.read_buf(&mut buf).await.unwrap();
+
+        if n == 0 {
+            break;
+        }
         if let Some(s) = buf.iter().position(|x| *x == b'\n') {
-            vote_caster(&mut buf, s, me.clone(), heartbeat_guard.clone()).await;
+            vote_caster(&mut buf, s, me.clone(), heartbeat_guard.clone(), &mut conn).await;
         }
     }
 }
 
-pub async fn vote_caster(buf: &mut BytesMut, pos: usize, me: Arc<Mutex<ThisNode>>, heartbeat_guard: Arc<Mutex<Instant>>) {
+pub async fn vote_caster(
+    buf: &mut BytesMut,
+    pos: usize,
+    me: Arc<Mutex<ThisNode>>,
+    heartbeat_guard: Arc<Mutex<Instant>>,
+    stream: &mut TcpStream
+) {
     let node = me.lock().await;
     let bytes = buf.split_to(pos + 1);
     println!("{}", String::from_utf8(bytes.to_vec()).unwrap());
     let msg: Rpc = serde_json::from_slice(&bytes).unwrap();
     match msg {
         Rpc::RequestVote { term, candidate_id } => {
-            let address = &node.peers.get((candidate_id - 1) as usize).unwrap().addr;
-            let vote = serde_json::to_vec(&Rpc::VoteResponse { term, granted: true }).unwrap();
-            println!("{}",address);
-            let mut conn = TcpStream::connect(address).await.unwrap();
-            println!("Connected");
-            let _ = conn.write_all(&vote).await;
-        }, 
+            let mut vote = serde_json::to_vec(&Rpc::VoteResponse {
+                term,
+                granted: true,
+            })
+            .unwrap();
+            vote.push(b'\n');
+            stream.write_all(&vote).await.unwrap()
+        }
         Rpc::Hearbeat { term, leader_id } => {
             let mut heartbeat = heartbeat_guard.lock().await;
             *heartbeat = Instant::now();
         }
-        _ => {
-        }
+        _ => {}
     }
-}   
+}
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,47 @@ use std::{clone, ptr::read, sync::Arc, time::Duration};
 
 use bytes::{Bytes, BytesMut};
 use rand::RngExt;
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}, sync::Mutex, time::{Instant, sleep}};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+    sync::Mutex,
+    time::{Instant, sleep},
+};
 
-use crate::{connection::{connect_to_peers, handle_connection}, distributed::request_reader, types::{Peer, Role::{self, Candidate, Follower}, Rpc, ThisNode}};
+use crate::{
+    connection::{connect_to_peers, handle_connection},
+    distributed::request_reader,
+    types::{
+        Peer,
+        Role::{self, Candidate, Follower},
+        Rpc, ThisNode,
+    },
+};
 
-
-pub mod types;
-pub mod distributed;
 pub mod connection;
+pub mod distributed;
+pub mod types;
 
 #[tokio::main]
 async fn main() {
     let heartbeat = Arc::new(Mutex::new(Instant::now()));
 
     let peers = vec![
-        Peer { id: 1, addr: "127.0.0.1:7000".into(), conn: None },
-        Peer { id: 2, addr: "127.0.0.1:7001".into(), conn: None },
-        Peer { id: 3, addr: "127.0.0.1:7002".into(), conn: None },
+        Peer {
+            id: 1,
+            addr: "127.0.0.1:7000".into(),
+            conn: None,
+        },
+        Peer {
+            id: 2,
+            addr: "127.0.0.1:7001".into(),
+            conn: None,
+        },
+        Peer {
+            id: 3,
+            addr: "127.0.0.1:7002".into(),
+            conn: None,
+        },
     ];
 
     let me = Arc::new(Mutex::new(ThisNode {
@@ -28,13 +52,12 @@ async fn main() {
         peers: peers,
         last_heartbeat: Instant::now(),
         voted_for: Some(3),
-        current_leader: 1
+        current_leader: 0,
     }));
     let node = me.clone();
 
-
     let listener = TcpListener::bind("127.0.0.1:7002").await.unwrap();
-    
+
     tokio::spawn(handle_connection(listener, me.clone(), heartbeat.clone()));
 
     let connector = tokio::spawn(connect_to_peers(me.clone()));
@@ -44,7 +67,6 @@ async fn main() {
     println!("Finished Setup");
 
     let watchdog = tokio::spawn(workdogfn(heartbeat.clone(), me.clone()));
-    
 
     let heartbeat = tokio::spawn(async move {
         loop {
@@ -56,22 +78,26 @@ async fn main() {
     let _ = tokio::join!(watchdog, heartbeat);
 }
 
-
 pub async fn send_heartbeat(me: Arc<Mutex<ThisNode>>) {
     let mut node = me.lock().await;
     let leader_id = node.id.clone();
-    let msg = Rpc::Hearbeat { term: node.current_term, leader_id: leader_id };
-    let bytes = serde_json::to_vec(&msg).unwrap();
+    let msg = Rpc::Hearbeat {
+        term: node.current_term,
+        leader_id: leader_id,
+    };
+    let mut bytes = serde_json::to_vec(&msg).unwrap();
+    bytes.push(b'\n');
     if node.role == Role::Leader {
         for i in node.peers.iter_mut() {
             if i.id == leader_id {
                 continue;
             }
+            println!("Writin to {}", i.addr);
             let _ = i.conn.as_mut().unwrap().write_all(&bytes).await;
+            sleep(Duration::from_secs(1)).await;
         }
     }
 }
-
 
 async fn workdogfn(heartbeat_guard: Arc<Mutex<Instant>>, me: Arc<Mutex<ThisNode>>) {
     let mut x;
@@ -100,14 +126,18 @@ pub async fn election(me: Arc<Mutex<ThisNode>>) {
     let mut me_guard = me.lock().await;
     me_guard.current_term += 1;
     me_guard.role = Candidate;
-    let mut total_votes = 1;
-    let vote = Rpc::RequestVote{term:me_guard.current_term,candidate_id:me_guard.id};
+    let mut total_votes = 0;
+    let vote = Rpc::RequestVote {
+        term: me_guard.current_term,
+        candidate_id: me_guard.id,
+    };
     for i in me_guard.peers.iter() {
         if i.id == me_guard.id || i.id == me_guard.current_leader {
             continue;
         }
         send_request(&i.addr, &vote, &mut buf).await;
         check_vote(&mut buf, &mut total_votes).await;
+        println!("{}", total_votes);
         if total_votes >= 2 {
             break;
         }
@@ -123,23 +153,23 @@ pub async fn send_request(address: &String, vote: &Rpc, buf: &mut BytesMut) {
     let _ = connection.write_all(&bytes).await;
     println!("Wrote to {}", address);
     loop {
-        let _ = connection.read(buf).await;
-        if let Some(_) = buf.iter().position(|x| *x == b'n') {
-            return 
+        let _ = connection.read_buf(buf).await;
+        if let Some(_) = buf.iter().position(|x| *x == b'\n') {
+            return;
         }
     }
 }
 
 pub async fn check_vote(buf: &mut BytesMut, total_votes: &mut u64) {
     let bytes = buf.split();
-    let vote: Rpc =  serde_json::from_slice(&bytes).unwrap();
+    println!("{}", String::from_utf8(bytes.to_vec()).unwrap());
+    let vote: Rpc = serde_json::from_slice(&bytes).unwrap();
     match vote {
         Rpc::VoteResponse { term, granted } => {
             if granted == true {
                 *total_votes += 1
             }
-        },
+        }
         _ => {}
     }
-
 }
